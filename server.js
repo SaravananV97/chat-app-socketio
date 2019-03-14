295fb2760041e51a133643ceb29c345b9bca65b3
Saravanan_V <saravananv124@gmail.com>
2019-03-14 23:50:20 +0530

    deployment fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === "production"){
 
   app.use(express.static("Frontend/build"));
 
-  app.get("*", (req,res) => {
+  app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "Frontend", "build", "index.html"));
   });
 
