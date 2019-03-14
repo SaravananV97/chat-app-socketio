4304a47db9f8fd355699a103eb6e86996e484c4a
Saravanan_V <saravananv124@gmail.com>
2019-03-15 01:11:54 +0530

    deployment fix

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,7 +9,7 @@ import HOC from "./Components/Hoc/hoc";
 import * as actionCreator from "./Store/Action/actionCreators";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
-
+console.log("port..." + process.env.PORT);
 const socket = io(`https://pacific-ravine-98350.herokuapp.com/${process.env.PORT}`);
 
 class App extends Component {
